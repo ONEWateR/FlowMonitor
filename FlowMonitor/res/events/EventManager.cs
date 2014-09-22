c69cea55b5ffc2b45b7687f1a48a1e99901fb86c
ONEWateR <bawgame1993@gmail.com>
2014-09-22 19:16:36 +0800

    [Function] 样式移植

---
+++ FlowMonitor/res/events/EventManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace onewater.flowmonitor.res.events
+{
+    
+    public partial class EventManager : ResourceDictionary
+    {
+        public static Window active_window = null;
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            win.Close();
+        }
+
+        private void Min_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            win.WindowState = WindowState.Minimized;
+        }
+
+
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+                win.DragMove();
+            }
+        }
+    }
+}
---
+++ FlowMonitor/res/events/ImageButton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace onewater.flowmonitor.res.events
+{
+
+    public class ImageButton : Button
+    {
+        public static readonly DependencyProperty MyMoverBrushProperty;
+        public static readonly DependencyProperty MyEnterBrushProperty;
+        public Brush MyMoverBrush
+        {
+            get
+            {
+                return base.GetValue(ImageButton.MyMoverBrushProperty) as Brush;
+            }
+            set
+            {
+                base.SetValue(ImageButton.MyMoverBrushProperty, value);
+            }
+        }
+        public Brush MyEnterBrush
+        {
+            get
+            {
+                return base.GetValue(ImageButton.MyEnterBrushProperty) as Brush;
+            }
+            set
+            {
+                base.SetValue(ImageButton.MyEnterBrushProperty, value);
+            }
+        }
+        static ImageButton()
+        {
+            ImageButton.MyMoverBrushProperty = DependencyProperty.Register("MyMoverBrush", typeof(Brush), typeof(ImageButton), new PropertyMetadata(null));
+            ImageButton.MyEnterBrushProperty = DependencyProperty.Register("MyEnterBrush", typeof(Brush), typeof(ImageButton), new PropertyMetadata(null));
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton), new FrameworkPropertyMetadata(typeof(ImageButton)));
+        }
+        public ImageButton()
+        {
+            base.Content = "";
+        }
+    }
+}
